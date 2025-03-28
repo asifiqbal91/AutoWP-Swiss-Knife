44332f0b50ad8b3a2a3d8e6b0c83a35d8430a93c
Asif Iqbal <asif@adommo.com>
2025-03-28 23:50:48 +0600

    Update:

--- classes/Astra.php
+++ classes/Astra.php
@@ -61,11 +61,45 @@ class Astra {
 
 		$astra_settings = get_option( 'astra-settings' );
 		$astra_settings['global-color-palette']['palette'] = $parameters['colorPalette'];
+
 		$astra_settings['body-font-family'] = $parameters['fonts']['body'];
 		$astra_settings['headings-font-family'] = $parameters['fonts']['heading'];
 
 		$astra_settings['header-desktop-items']['primary']['primary_center'] = ['menu-1'];
 		$astra_settings['header-desktop-items']['primary']['primary_right'] = ['widget-1'];
+
+		$astra_settings['footer-desktop-items']['popup']['popup_content'] = [];
+		$astra_settings['footer-desktop-items']['layouts']['primary']['column'] = 4;
+		$astra_settings['footer-desktop-items']['layouts']['primary']['layout'] = [
+			'mobile' => 'full',
+			'tablet' => '4-equal',
+			'desktop' => '4-equal'
+		];
+		$astra_settings['hb-footer-column'] = '4';
+		$astra_settings['hb-footer-layout'] = [
+			'desktop' => '4-equal',
+			'tablet' => '4-equal',
+			'mobile' => 'full',
+			'flag' => false,
+		];
+
+		$astra_settings['footer-desktop-items']['primary']['primary_1'] = ['html-1', 'social-icons-1'];
+		$astra_settings['footer-html-1'] = '<h5>About Us</h5>\nQuam quam lacus, amet lorem eu nunc, eget dui libero et faucibus facilisis sed odio suspendisse';
+		$astra_settings['footer-html-1-alignment'] = [
+			'desktop' => 'left',
+			'tablet' => 'center',
+			'mobile' => 'center',
+		];
+		$astra_settings['footer-social-1']['enable'] = false;
+		$astra_settings['footer-social-1-alignment']['enable'] = [
+			'desktop' => 'right',
+			'tablet' => 'center',
+			'mobile' => 'center',
+		];
+
+		$astra_settings['footer-desktop-items']['primary']['primary_2'] = ['widget-1'];
+		$astra_settings['footer-desktop-items']['primary']['primary_3'] = ['widget-2'];
+		$astra_settings['footer-desktop-items']['primary']['primary_4'] = ['widget-3'];
 
 		$astra_settings['footer-copyright-editor'] = 'Copyright [copyright] [current_year] [site_title] | Powered by [site_title]';
 
